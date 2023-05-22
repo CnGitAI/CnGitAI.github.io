6d1b8d435272bf12e525349576d70cb14708fbd0
awen <2205439947@qq.com>
2023-05-22 22:46:05 +0800

    打字机状态改成不间断效果

--- .vuepress/public/js/common.js
+++ .vuepress/public/js/common.js
@@ -1,17 +1,21 @@
-export function autoInput(id,des){
+export function autoInput(id, des) {
   let divTyping = document.getElementById(id)
   let i = 0,
     timer = 0,
-    str = des
-  function typing () {
-    if (i <= str.length) {
+    str = des,
+    reverse = false // 标志变量，控制是否反向输出字符串
+  function typing() {
+    if (!reverse && i < str.length) {
       divTyping.innerHTML = str.slice(0, i++) + '_'
-      timer = setTimeout(typing, 200)
-    }
-    else {
-      divTyping.innerHTML = str//结束打字,移除 _ 光标
+      timer = setTimeout(typing, 300)
+    } else if (reverse && i > 0) {
+      divTyping.innerHTML = str.slice(0, i--) + '_'
+      timer = setTimeout(typing, 300)
+    } else {
+      reverse = !reverse // 反转标志变量
       clearTimeout(timer)
+      typing()
     }
   }
-typing()
+  typing()
 }
