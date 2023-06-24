cbb29e4b5be92241bd8ea56f8d7f6b5544a9cefe
awen <2205439947@qq.com>
2023-06-24 23:02:56 +0800

    update config file

--- api/proxy.js
+++ api/proxy.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
   // 代理目标地址
   // 这里使用 blogImage 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/blogImage')) {
-    target = 'http://rwlot91gu.hd-bkt.clouddn.com/blogImage'
+    target = 'http://rwlot91gu.hd-bkt.clouddn.com/'
   }
 
   // 创建代理对象并转发请求
@@ -16,7 +16,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/blogImage`
       // 例如 /blogImage/202306211922891.jpg 将被转发到 http://rwlot91gu.hd-bkt.clouddn.com//blogImage/202306211922891.jpg
-      '^/blogImage/': '/'
+      // '^/blogImage/': '/'
     }
   })(req, res)
 }
